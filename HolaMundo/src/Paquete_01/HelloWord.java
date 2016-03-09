61a7bba69ba459f1ad940ff7938ff0bb87127006
Juan <juaniyoalm@gmail.com>
2016-03-09 18:13:32 +0100

    Subida HolaMundo

---
+++ HolaMundo/src/Paquete_01/HelloWord.java
@@ -0,0 +1,12 @@
+package Paquete_01;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola Mundo");
+		
+	}
+
+}
